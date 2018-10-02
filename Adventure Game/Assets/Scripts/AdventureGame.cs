75a4303886800a9bfed2497b55bd27070f698f60
Daniel Vega <daniel_dvs95@hotmail.com>
2018-10-01 22:14:51 -0500

    Initial Commit
    Initial Commit

---
+++ Adventure Game/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+
+    State currentState;
+
+    void Start()
+    {
+        currentState = startingState;
+        storyText.text = currentState.GetStateStory();
+    }
+
+	void Update()
+	{
+        ManageState();
+	}
+
+    void ManageState()
+    {
+        var nextStates = currentState.GetNextStates();
+        if (nextStates != null)
+        {
+            for (int index = 0; index < nextStates.Length; index++) 
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+                {
+                    currentState = nextStates[index];
+                }
+            }
+        }
+        storyText.text = currentState.GetStateStory();
+    }
+}
